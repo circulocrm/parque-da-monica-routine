5317d1eb3a75bf3c66a733593b90ed8aeb629552
Fellipe Lorram <fellipelorram@gmail.com>
2022-02-23 10:30:01 -0300

    Add jest, and eslint configuration

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get('/', (request: Request, response: Response): Response => {
-  return response.json({ message: 'It is working' })
-});
+app.get('/', (request: Request, response: Response): Response => response.json({ message: 'It is working' }));
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
