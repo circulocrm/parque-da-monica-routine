6ebf3b3aad178559d787b2e64eb0fda359c46280
Fellipe Lorram <fellipelorram@gmail.com>
2023-01-05 21:14:42 -0300

    Adding schedule function

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,33 @@
 import mongoose from 'mongoose';
 import app from './app';
 import transferDataUseCase from './UseCases/TransferUseCase';
+import { scheduleFunction } from './Utils/scheduleFunction';
 
 require('dotenv').config();
 
 declare const process: {
   env: {
-    CONNECTIONSTRING: string
-    PORT: number
-  }
+    CONNECTIONSTRING: string;
+    PORT: number;
+  };
 };
 
-mongoose.connect(process.env.CONNECTIONSTRING)
+mongoose
+  .connect(process.env.CONNECTIONSTRING)
   .then(() => {
     app.emit('Conectado');
   })
   .catch((e) => console.log(e));
 
-const executeAPI = async () => {
-  await transferDataUseCase.execute();
-  setInterval(async () => { await transferDataUseCase.execute(); }, 1000 * 60 * 60 * 24)
-}
 
 app.on('Conectado', () => {
   app.listen(process.env.PORT || 3333, async () => {
-    console.log('http://localhost:3333')
-    await executeAPI();
+    console.log('http://localhost:3333');
+    scheduleFunction(9, 30, async () => {
+      await transferDataUseCase.execute();
+    });
+    scheduleFunction(16, 30, async () => {
+      await transferDataUseCase.execute();
+    });
   });
 });
